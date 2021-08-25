d4ab9d6160c668c40a5a5d960cf098f8b70e8400
SimonvdFliert <Simonvdfliert@hotmail.com>
2021-08-25 14:17:40 +0200

    Update site.js

--- js/site.js
+++ js/site.js
@@ -1,4 +1,20 @@
-// Call Hello World
-function helloWorld(){
-    alert("hello World");
+// Get the values from website
+function getValues(){
+    let loanAmount = document.getElementById("LoanAmount").value;
+    let MonthlyPayments = document.getElementById("MonthlyPayments").value;
+    let InterestRate = document.getElementById("InterestRate").value;
+
+    let totalMonthlyPayment, InterestPayment, principalPayment, remainingBalance = calculateMoney(loanAmount, MonthlyPayments, InterestRate);
+    
+}
+
+//Calculate scores
+function calculateMoney(loanAmount, MonthlyPayments, InterestRate){
+
+    totalMonthlyPayment = ((loanAmount) * (InterestRate /1200) / (1 - (1 + InterestRate / 1200)**(-MonthlyPayments)))
+    InterestPayment = 
+
+
+
+
 }
